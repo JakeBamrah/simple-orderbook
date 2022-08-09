d595326b648b05059f25e981cc614be6418cca9a
Jake Bamrah <login@bamrah.sh>
2022-08-09 21:08:07 +0000

    integrate google test suite into unit tests

--- tests/unittests.cc
+++ tests/unittests.cc
@@ -1,21 +1,25 @@
 #include <assert.h>
 #include <functional>
+#include <gtest/gtest.h>
 
 #include "../src/orderbook.cc"
 
 using std::function;
 
 
-void testOrderBook()
+// TODO: write market order and cancel order tests
+
+TEST(OrderBookTest, TestOrderBookInitialize)
 {
     OrderBook orderbook;
-    assert(orderbook.size() == 0);
-    assert(orderbook.getInsideBid() == 0);
-    assert(orderbook.getInsideAsk() == 0);
+    ASSERT_EQ(orderbook.size(), 0);
+    ASSERT_EQ(orderbook.getInsideBid(), 0);
+    ASSERT_EQ(orderbook.getInsideAsk(), 0);
 }
 
-void testOrderBookBid()
+TEST(OrderBookTest, TestOrderBookBidCreate)
 {
+
     OrderBook orderbook;
 
     function<bool(double, double)> bid_compare;
@@ -25,11 +29,11 @@ void testOrderBookBid()
     orderbook.sendLimitOrder(QuoteType::BID, 1, 90, bid_compare);
     orderbook.sendLimitOrder(QuoteType::BID, 1, 100, bid_compare);
 
-    assert(orderbook.size() == 3);
-    assert(orderbook.getInsideBid() == 10000);
+    ASSERT_TRUE(orderbook.size() == 3);
+    ASSERT_TRUE(orderbook.getInsideBid() == 10000);
 }
 
-void testOrderBookAsk()
+TEST(OrderBookTest, TestOrderBookAskCreate)
 {
     OrderBook orderbook;
 
@@ -40,11 +44,11 @@ void testOrderBookAsk()
     orderbook.sendLimitOrder(QuoteType::ASK, 1, 90, ask_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 1, 100, ask_compare);
 
-    assert(orderbook.size() == 3);
-    assert(orderbook.getInsideAsk() == 8000);
+    ASSERT_TRUE(orderbook.size() == 3);
+    ASSERT_TRUE(orderbook.getInsideAsk() == 8000);
 }
 
-void testOrderBookLimitExecution()
+TEST(OrderBookTest, TestSingleBidAskExecute)
 {
     OrderBook orderbook;
 
@@ -57,19 +61,38 @@ void testOrderBookLimitExecution()
     orderbook.sendLimitOrder(QuoteType::BID, 10, 100, bid_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 10, 100, ask_compare);
 
-    assert(orderbook.size() == 0);
-    assert(orderbook.getInsideBid() == 0);
-    assert(orderbook.getInsideAsk() == 0);
+    ASSERT_TRUE(orderbook.size() == 0);
+    ASSERT_TRUE(orderbook.getInsideBid() == 0);
+    ASSERT_TRUE(orderbook.getInsideAsk() == 0);
+}
+
+TEST(OrderBookTest, TestMultipleAskBidExecute)
+{
+    OrderBook orderbook;
+
+    function<bool(double, double)> bid_compare;
+    function<bool(double, double)> ask_compare;
+    ask_compare = orderbook.buildCompareCallback(QuoteType::ASK);
+    bid_compare = orderbook.buildCompareCallback(QuoteType::BID);
 
     // test multiple ask to bid execution
-    orderbook = OrderBook();
     orderbook.sendLimitOrder(QuoteType::BID, 20, 100, bid_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 10, 100, ask_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 10, 100, ask_compare);
 
-    assert(orderbook.size() == 0);
-    assert(orderbook.getInsideBid() == 0);
-    assert(orderbook.getInsideAsk() == 0);
+    ASSERT_TRUE(orderbook.size() == 0);
+    ASSERT_TRUE(orderbook.getInsideBid() == 0);
+    ASSERT_TRUE(orderbook.getInsideAsk() == 0);
+}
+
+TEST(OrderBookTest, TestMultipleBidAskExecute)
+{
+    OrderBook orderbook;
+
+    function<bool(double, double)> bid_compare;
+    function<bool(double, double)> ask_compare;
+    ask_compare = orderbook.buildCompareCallback(QuoteType::ASK);
+    bid_compare = orderbook.buildCompareCallback(QuoteType::BID);
 
     // test multiple bid to ask execution
     orderbook = OrderBook();
@@ -77,40 +100,49 @@ void testOrderBookLimitExecution()
     orderbook.sendLimitOrder(QuoteType::BID, 10, 100, bid_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 20, 100, ask_compare);
 
-    assert(orderbook.size() == 0);
-    assert(orderbook.getInsideBid() == 0);
-    assert(orderbook.getInsideAsk() == 0);
+    ASSERT_TRUE(orderbook.size() == 0);
+    ASSERT_TRUE(orderbook.getInsideBid() == 0);
+    ASSERT_TRUE(orderbook.getInsideAsk() == 0);
+}
+
+TEST(OrderBookTest, TestPartialAskExecution)
+{
+    OrderBook orderbook;
+
+    function<bool(double, double)> bid_compare;
+    function<bool(double, double)> ask_compare;
+    ask_compare = orderbook.buildCompareCallback(QuoteType::ASK);
+    bid_compare = orderbook.buildCompareCallback(QuoteType::BID);
 
     // test partial ask execution
     orderbook = OrderBook();
     orderbook.sendLimitOrder(QuoteType::BID, 10, 100, bid_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 20, 100, ask_compare);
 
-    assert(orderbook.size() == 1);
-    assert(orderbook.getInsideBid() == 0);
-    assert(orderbook.getInsideAsk() == 10000);
-    assert(orderbook.getInsideAskSize() == 10);
+    ASSERT_EQ(orderbook.size(), 1);
+    ASSERT_EQ(orderbook.getInsideBid(), 0);
+    ASSERT_EQ(orderbook.getInsideAsk(), 10000);
+    ASSERT_EQ(orderbook.getInsideAskSize(), 10);
+}
+
+TEST(OrderBookTest, TestPartialBidExecution)
+{
+    OrderBook orderbook;
+
+    function<bool(double, double)> bid_compare;
+    function<bool(double, double)> ask_compare;
+    ask_compare = orderbook.buildCompareCallback(QuoteType::ASK);
+    bid_compare = orderbook.buildCompareCallback(QuoteType::BID);
 
     // test partial bid execution
-    orderbook = OrderBook();
     orderbook.sendLimitOrder(QuoteType::BID, 10, 80, bid_compare);
     orderbook.sendLimitOrder(QuoteType::BID, 10, 90, bid_compare);
     orderbook.sendLimitOrder(QuoteType::BID, 15, 90, bid_compare);
     orderbook.sendLimitOrder(QuoteType::ASK, 40, 90, ask_compare);
 
-    assert(orderbook.size() == 2);
-    assert(orderbook.getInsideBid() == 8000);
-    assert(orderbook.getInsideAsk() == 9000);
-    assert(orderbook.getInsideAskSize() == 15);
-    assert(orderbook.getInsideBidSize() == 10);
-}
-
-// TODO: write market order and cancel order tests
-
-int main()
-{
-    testOrderBook();
-    testOrderBookBid();
-    testOrderBookAsk();
-    testOrderBookLimitExecution();
+    ASSERT_EQ(orderbook.size(), 2);
+    ASSERT_EQ(orderbook.getInsideBid(), 8000);
+    ASSERT_EQ(orderbook.getInsideAsk(), 9000);
+    ASSERT_EQ(orderbook.getInsideAskSize(), 15);
+    ASSERT_EQ(orderbook.getInsideBidSize(), 10);
 }
