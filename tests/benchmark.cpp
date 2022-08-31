6402419badfc6170dcb726d8be1351b614c4a217
Jake Bamrah <login@bamrah.sh>
2022-08-31 20:41:29 +0000

    add data file i/o checks and simplify benchmarking

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -13,6 +13,29 @@
 #define __NUM_ORDERS__ 200000
 #define durationMs(a) std::chrono::duration_cast<std::chrono::milliseconds>(a);
 
+
+/*
+ * Generates order data (quote type, price and quantity) that is used to create
+ * an order. Writing the test data to a file allows for more reproducible
+ * testing while avoiding compiler optimisations which may impact benchmarking.
+ */
+void generateTestData(int num_orders)
+{
+    std::ofstream file{"order_data.txt"};
+    for (int i = 0; i <= num_orders; i++)
+    {
+        // multiply by a random float to get a price
+        // use +1 to avoid price and quantities of 0
+        double price{((rand() % 10) + 1) * 1.34};
+        int quantity{((rand() % 10) + 1) * 100};
+        int is_buy{(i % 2) == 0};
+        file << is_buy << " " << price << " " << quantity << std::endl;
+    }
+    file.close();
+
+    return;
+}
+
 /*
  * Splits each line of order data into is_buy, price and quantity.
  * is_buy = 1 = QuoteType::BID
@@ -33,14 +56,21 @@ std::vector<T> split(std::string& s, char delim)
 }
 
 /* Pull in order data from generated file and convert to Order objects. */
-Order** getOrdersFromFile(OrderBook& orderbook)
+Order** getOrdersFromFile(OrderBook& orderbook, int num_orders)
 {
-    Order** orders = new Order*[__NUM_ORDERS__ + 1];
+    Order** orders = new Order*[num_orders + 1];
 
     uint i = 0;
     std::string line;
     std::ifstream file{"order_data.txt"};
-    while (std::getline(file, line) && i <= __NUM_ORDERS__)
+    if (!file.is_open())
+    {
+        std::cout << "File does not exist. Generating order data! \n";
+        generateTestData(num_orders);
+        file.open("order_data.txt");
+    }
+
+    while (std::getline(file, line) && i <= num_orders)
     {
         // is_buy, price, quantity
         std::vector<double> values = split<double>(line, ' ');
@@ -61,11 +91,11 @@ Order** getOrdersFromFile(OrderBook& orderbook)
     return orders;
 }
 
-int run_test(OrderBook& orderbook, Order** orders)
+int run_test(OrderBook& orderbook, Order** orders, int num_orders)
 {
     auto start = std::chrono::steady_clock::now();
 
-    for (int i; i < __NUM_ORDERS__; i++)
+    for (int i; i < num_orders; i++)
     {
         Order order = **orders;
         auto compare = orderbook.buildCompareCallback(order.quote_type);
@@ -85,13 +115,18 @@ int run_test(OrderBook& orderbook, Order** orders)
 
 int main(int argc, const char* argv[])
 {
+    int num_orders = __NUM_ORDERS__;
+    if (argc > 1) {
+        num_orders = std::atoi(argv[1]);
+    }
+
     OrderBook orderbook{2};
-    Order** orders = getOrdersFromFile(orderbook);
-    auto dur = run_test(orderbook, orders);
+    Order** orders = getOrdersFromFile(orderbook, num_orders);
+    auto dur = run_test(orderbook, orders, num_orders);
     std::cout << "Time take: " << dur << "ms/n";
 
     // clean-up :)
-    for (uint i; i <= __NUM_ORDERS__; i++)
+    for (uint i; i <= num_orders; i++)
     {
         delete orders[i];
     }
@@ -99,4 +134,3 @@ int main(int argc, const char* argv[])
 
     return 0;
 }
-
